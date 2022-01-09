32f8b79f20c06501c6707a217d12b4364e501fc3
@50601_Harsha <hsonawane1998@gmail.com>
2022-01-09 19:55:10 +0530

    harsha

---
+++ src/main/java/com/example/java_jenkins_on_docker/App.java
@@ -0,0 +1,18 @@
+package com.example.java_jenkins_on_docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    
+    {
+      System.out.println(ifInputIsEven(24));
+    }
+    public static boolean ifInputIsEven(int number)
+    {
+    	return number%2==0;
+    }
+}
